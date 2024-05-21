fbb7c3574839b9ae5677bac2e0ecebac664e7be3
Skull-crusher44 <135740779+Skull-crusher44@users.noreply.github.com>
2024-05-21 12:31:16 +0530

    Update 0023-merge-k-sorted-lists.cpp
    Merge two sorted liked list concept

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,6 +10,7 @@
  */
 class Solution {
 public:
+
    ListNode*merge(ListNode*list1,ListNode*list2)
     {
         ListNode*tail=new ListNode(0);
@@ -40,8 +41,23 @@ public:
         return head->next;
     }
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if(lists.size()==0||lists[0]==NULL)
+        if(lists.size()==0)
         return NULL;
+
+        int i=0;
+        // 1--- check element exists or not in vector
+        //2--- then check ->that value is null or not (it swap the conditions it will not work for lists=[[]])
+        while(i<lists.size()&&lists[i]==NULL)
+        {
+            i++;
+        }
+        // if i==lists.size()-> this means all elements of vector are null ( lists =[[],[],[],[]] )
+        if(i==lists.size())                                                        // 0  1  2  3 i
+        {
+            return NULL;
+        }
+        //merge two list again and again 
+        // head will store the head pointer of two merged list then head will be merged to next list
         ListNode*head=lists[0];
         for(int i=0;i<lists.size()-1;i++)
         {
