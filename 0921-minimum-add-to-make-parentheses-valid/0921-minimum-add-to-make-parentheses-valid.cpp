57cf60e88d5a36589e82db7c0aed4256781cb320
Skull-crusher44 <135740779+Skull-crusher44@users.noreply.github.com>
2024-05-23 18:40:21 +0530

    Time: 0 ms (100%), Space: 7.5 MB (41.2%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -2,35 +2,21 @@ class Solution {
 public:
     int minAddToMakeValid(string s) {
 
-  stack<char>st;
-  for(int i=0;i<s.length();i++)
-  {
-    char ch= s[i];
-    if(ch=='(')                         //if ch ='(' then push ch into stack  & wait for closing braces ')'
-      st.push(ch);
-    else
-    {
-      if(!st.empty()&&st.top()=='(')     // if i get ')' then first check wheter stack is empty or not 
-        st.pop();                        //then i will check whether top element is '(' or not  (string ="(())") 
-        else
-        st.push(ch);                       // if ch= ')'===> either top is ')' or stack is empty ==> then push ')'
-    }                                      // string =" ))()()"or"(())))"
-  }
-//   int a=0,b=0;
-//   while(!st.empty())
-//   {
-    
-//     char ch = st.top();
-//     if(ch=='(')
-//     a++;
-//     else
-//     b++;
-//     st.pop();
-//   }
-//   int ans = a+b;
-  return st.size();
-
-
+        stack<char> st;
+        int count=0;            //store no of closing parenthesis
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s[i];
+            if (ch == '(') 
+                st.push(ch);
+            else {
+                if (st.empty())
+                    count++;
+                else
+                    st.pop(); 
+                }
+        }
+        int no_of_open_parenth=st.size();
+        return (count+no_of_open_parenth);
         
     }
 };
