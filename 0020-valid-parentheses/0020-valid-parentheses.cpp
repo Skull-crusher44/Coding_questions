23edc86378f21b5e04734b410ef254e15f9e584d
Skull-crusher44 <135740779+Skull-crusher44@users.noreply.github.com>
2024-05-23 20:14:39 +0530

    Time: 0 ms (100%), Space: 7.7 MB (31.72%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char>st;
-        for(int i=0;i<s.length();i++)
-        {
-            if(s[i]=='('||s[i]=='{'||s[i]=='[')
-            st.push(s[i]);
-            else if(!st.empty())  // if i want to check with top element then stack must not be empty
+        stack<char> st;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+                st.push(s[i]);
+            else // if i want to check with top element then stack must not be  empty
             {
-                 if (s[i]==')'&&st.top()=='(')
-                st.pop();
-                else if(s[i]=='}'&&st.top()=='{')
-                st.pop();
-                else if(s[i]==']'&&st.top()=='[')
-                st.pop();
-                else 
-                return false;  // s="(])" if we have closing parenthesis but we don't find any corresponding opening parenthesis
+                if (st.empty())   // s="()]" if stack is empty and we encounter  closing parenthesis
+                    return false; // this shows ==> there is no chance of getting corresponding opening parenthesis
+                if (s[i] == ')' && st.top() == '(')
+                    st.pop();
+                else if (s[i] == '}' && st.top() == '{')
+                    st.pop();
+                else if (s[i] == ']' && st.top() == '[')
+                    st.pop();
+                else
+                    return false; // s="(])" if we have closing parenthesis but we don't find any corresponding opening parenthesis
             }
-            else
-            return false;               // s="()]" if stack is empty and we encounter closing parenthesis
-        }                               // this shows ==> there is no chance of getting corresponding opening parenthesis
-        if(st.empty())     
-        return true;        // s="((){}[])"  after this stack will be empty 
+        }
+        if (st.empty())
+            return true; // s="((){}[])"  after this stack will be empty
         else
-        return false; // s="((({}[]"   after this stack will have ((( and stack is not empty ==>not vlaid parenheses 
+            return false; // s="((({}[]"   after this stack will have ((( and stack is not empty ==>not vlaid parenheses
     }
 };
