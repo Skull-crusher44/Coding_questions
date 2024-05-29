35f6dab2793fbc51a38af90e4201ad377ef3064e
Skull-crusher44 <135740779+Skull-crusher44@users.noreply.github.com>
2024-05-29 14:13:47 +0530

    Time: 15 ms (77.29%), Space: 20 MB (69.66%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,30 +1,70 @@
+//solution 1 using another stack
+// class MinStack {
+// public:
+//     stack<int>s;    // it will store min elemnets
+//     stack<int>p;  // it will store history of min elements
+//     MinStack() {
+        
+//     }
+    
+//     void push(int val) {    //s=> 2 5 8 1 4 5
+//         p.push(val);        //p=> 2 2 2 1 1 1(min elements in stack s)
+//         if(s.empty())
+//         s.push(val);
+//         else
+//         s.push(min(val,s.top()));
+//     }
+    
+//     void pop() {
+//         p.pop();
+//         s.pop();
+//     }
+    
+//     int top() {
+//         return p.top();
+//     }
+    
+//     int getMin() {
+//         return s.top();
+//     }
+// };
+
+//solution 2=> Here we putting element then minimum element from stack 
 class MinStack {
 public:
-    stack<int>s;
-    stack<int>p;
+stack<int>s;
+int minEle;
     MinStack() {
         
     }
     
     void push(int val) {
-        p.push(val);
-        if(s.empty())
-        s.push(val);
-        else
-        s.push(min(val,s.top()));
+        if(!s.empty())
+           minEle=min(s.top(),val);
+           else
+           minEle=val;
+           
+           s.push(val);           
+           s.push(minEle);
     }
     
     void pop() {
-        p.pop();
-        s.pop();
+           s.pop();
+           s.pop();
+         
     }
     
     int top() {
-        return p.top();
+        int top_min=s.top();
+        s.pop();
+        int top=s.top();
+        s.push(top_min);
+        return top;
+        
     }
     
     int getMin() {
-        return s.top();
+           return s.top();
     }
 };
 
