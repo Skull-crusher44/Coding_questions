e6f6877a9adf9df7fd551fda75df7858ef4ebb74
Skull-crusher44 <135740779+Skull-crusher44@users.noreply.github.com>
2024-07-04 03:01:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int>s;
+        int n= asteroids.size();
+        
+        for(int i=0;i<n;i++)
+        {
+            bool flag=true;
+            while(!s.empty()&&s.top()>0&&asteroids[i]<0)
+            {
+                if(s.top()+asteroids[i]==0)
+                {
+                    s.pop();
+                    flag=false;
+                }
+                else if(s.top() <= -asteroids[i])
+                s.pop();
+                else
+                {
+                    flag=false;
+                    break;
+                }
+            }
+            if(flag)
+            s.push(asteroids[i]);
+        }
+
+        vector<int>ans(s.size(),0);
+        while(!s.empty())
+        {
+            ans[s.size()-1]=s.top();
+            s.pop();
+        }
+        return ans;
+    }
+};
