fbc62f5427834462da9e3bdf3ca8dca411238a9e
Skull-crusher44 <135740779+Skull-crusher44@users.noreply.github.com>
2024-07-25 16:01:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -6,36 +6,37 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-TreeNode* createtree(vector<int>inorder,vector<int>preorder,int index, int start,int end)
-    {
-        if(start>end)
-        return NULL;
-        
-        //create a node 
-        TreeNode*root=new TreeNode(preorder[index]);
-        
-        // find the position of index element from preorder in inorder array 
-        int pos;
-        for(int i=start;i<end;i++)
-        {
-            if(inorder[i]==preorder[index])
-            {
-                pos = i;
-                break;
-            }
-        }
-        
-        // recursion of left and right subtree 
-        root->left = createtree(inorder,preorder,index+1,start,pos-1);
-        root->right = createtree(inorder,preorder,index+(pos-start+1),pos+1,end);
+    TreeNode* createtree(vector<int> inorder, vector<int> preorder, int index,
+                         int start, int end,unordered_map<int,int>mp) {
+                            // index = first element of preoder vec
+        if (start > end)
+            return NULL;
+
+        // create a node
+        TreeNode* root = new TreeNode(preorder[index]);
+
+        // find the position of index element from preorder in inorder array
+        int pos = mp[preorder[index]];
+    
+
+        // recursion of left and right subtree
+        root->left = createtree(inorder, preorder, index + 1, start, pos - 1,mp);
+        root->right = createtree(inorder, preorder, index + (pos - start + 1),
+                                 pos + 1, end,mp);
         return root;
     }
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        return createtree(inorder,preorder,0,0,inorder.size()-1);
+        unordered_map<int ,int>mp;
+        for(int i=0;i<inorder.size();i++)
+        {
+            mp[inorder[i]]=i;
+        }
+        return createtree(inorder, preorder, 0, 0, inorder.size() - 1,mp);
     }
 };
