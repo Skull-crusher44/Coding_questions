b8861abdb09c547db50d4cf77aeff5925e1c6d80
Skull-crusher44 <135740779+Skull-crusher44@users.noreply.github.com>
2024-10-16 02:36:47 +0530

    Time: 59 ms (46.76%), Space: 34.2 MB (86.81%) - LeetHub

--- 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
@@ -1,52 +1,41 @@
 class Solution {
 public:
-    int countPaths(int v, vector<vector<int>>& roads) {
-    // Create adjacency list
-    vector<pair<int, int>> adj[v];
-    for (const auto& road : roads) {
-        adj[road[0]].push_back({road[1], road[2]});
-        adj[road[1]].push_back({road[0], road[2]}); // Since the roads are bidirectional
-    }
-
-    // Min-heap priority queue, where each pair is {distance, node}
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
-    q.push({0, 0}); // {distance, start node}
-
-    vector<int> dist(v, INT_MAX); // Distance array
-    dist[0] = 0;
-
-    vector<int> ways(v, 0); // To count the number of ways to reach each node
-    ways[0] = 1; // There's one way to reach the start node
-
-    while (!q.empty()) {
-        int currDist = q.top().first;
-        int currNode = q.top().second;
-        q.pop();
-
-        // If the popped distance is already greater than the current known distance, skip
-        if (currDist > dist[currNode]) {
-            continue;
+    int countPaths(int n, vector<vector<int>>& roads) {
+        int mod = 1e9+7;
+        vector<vector<pair<int, int>>> graph(n);
+        for(auto &road: roads) {
+            graph[road[0]].push_back({road[1], road[2]});
+            graph[road[1]].push_back({road[0], road[2]});
         }
-
-        // Explore the neighbors of the current node
-        for (auto& [neighbor, weight] : adj[currNode]) {
-            int newDist = currDist + weight;
-
-            // If a shorter path to the neighbor is found
-            if (newDist < dist[neighbor]) {
-                dist[neighbor] = newDist;
-                q.push({newDist, neighbor});
-                ways[neighbor] = ways[currNode]; // Set the number of ways to the current node
-            }
-            // If another path with the same shortest distance is found
-            else if (newDist == dist[neighbor]) {
-                ways[neighbor] = (ways[neighbor] + ways[currNode]) % 1000000007; // Add ways
+        
+        vector<long long> distance(n, LONG_MAX);
+        vector<int> ways(n, 0);
+        
+        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
+        pq.push({0, 0});
+        distance[0] = 0;
+        ways[0] = 1;
+        
+        while(!pq.empty()) {
+            pair<long long, int> t = pq.top();
+            pq.pop();
+            
+            for(auto &nbr: graph[t.second]) {
+                long long vert = nbr.first;
+                long long edge = nbr.second;
+                
+                if(distance[vert] > distance[t.second] + edge) {
+                    distance[vert] = distance[t.second] + edge;
+                    pq.push({distance[vert], vert});
+                    ways[vert] = ways[t.second] %mod;
+                }
+                else if(distance[vert] == t.first + edge) {
+                    ways[vert] += ways[t.second];
+                    ways[vert] %= mod;
+                }
             }
         }
+        
+        return ways[n-1];
     }
-
-    // Return the number of ways to reach the last node (v-1)
-    return ways[v-1];
-}
-
 };
