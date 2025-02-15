8604cb3955985a5b9765d0ac90dc7ad520893e39
Skull-crusher44 <135740779+Skull-crusher44@users.noreply.github.com>
2025-02-15 18:51:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int count0 =0,count1=0,count2=0;
-        for(int i =0 ; i < nums.size() ; i++)
+        int low=0,mid=0,high=nums.size()-1;
+
+        while(mid<=high)
         {
-            if(nums[i]==0)
-            count0++;
-            else if(nums[i]==1)
-            count1++;
-            else
-            count2++;
+            if(nums[mid]==0){
+                swap(nums[low],nums[mid]);
+                low++;
+                mid++;
+            }
+            else if (nums[mid]==1){
+                mid++;
+            }
+            else{
+                swap(nums[mid],nums[high]);
+                mid++;
+                high--;
+            }
         }
-        int i=0;
-        while(count0--)
-        nums[i++]=0;
-        while(count1--)
-        nums[i++]=1;
-        while(count2--)
-        nums[i++]=2;
 
     }
 };
