d79861f90c063957406be3de6f161e295e78c5a1
Skull-crusher44 <135740779+Skull-crusher44@users.noreply.github.com>
2025-03-02 23:29:54 +0530

    Time: 217 ms (12.56%), Space: 147.7 MB (25.72%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,26 +1,22 @@
 class Solution {
+    using int2=pair<int, int>; //(nums[i], i)
 public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        int maxi=INT_MIN;
-        int n=nums.size();
 
-        if(k==1)
-        {
-            return nums;
-        }
-        vector<int>ans(n-k+1,0);
-        for(int i=0;i<k;i++)
-        {
-            maxi=max(maxi,nums[i]);
-        }
-        ans[0]=maxi;
-        for(int i=k;i<n;i++)
-        {
-            if(nums[i]>=maxi)
-            {
-                 maxi=nums[i];
-            }
-            ans[i-k+1]=maxi;   
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n=nums.size();
+        priority_queue<int2> pq;
+
+        vector<int> ans(n-k+1);
+        for (int i=0; i<k; i++)
+            pq.push({nums[i], i});
+            
+        
+        ans[0]=pq.top().first;
+        for(int i=k; i<n; i++){
+            while(!pq.empty() && pq.top().second<=(i-k))
+                pq.pop(); //Pop up element not in the window
+            pq.push({nums[i], i});
+            ans[i-k+1]=pq.top().first;//Max element for this window
         }
         return ans;
     }
