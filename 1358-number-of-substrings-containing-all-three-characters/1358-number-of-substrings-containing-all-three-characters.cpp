b8a5fa298bfcaa2e8136c45dcc363fe7adb7ca6f
Skull-crusher44 <135740779+Skull-crusher44@users.noreply.github.com>
2025-03-11 18:55:51 +0530

    Time: 5 ms (76.93%), Space: 11.1 MB (35.03%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -1,22 +1,38 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        unordered_map<char,int>freq;
-        long long  left=0,right=0,res=0;
-        int n= s.length();
-        for(int i =0 ; i < s.length() ; i++){
-            
-            char ch=s[i];
-            freq[ch]++;
-            while(freq.size()==3)
-            {
-                res+=n-right;
-                if(--freq[s[left]]==0)
-                    freq.erase(s[left]);
+        int len = s.length();
+        int left = 0, right = 0;
+        // Track frequency of a, b, c
+        vector<int> freq(3, 0);
+        int total = 0;
+
+        while (right < len) {
+            // Add character at right pointer to frequency array
+            char curr = s[right];
+            freq[curr - 'a']++;
+
+            // While we have all required characters
+            while (hasAllChars(freq)) {
+                // All substrings from current window to end are valid
+                // Add count of these substrings to result
+                total += len - right;
+
+                // Remove leftmost character and move left pointer
+                char leftChar = s[left];
+                freq[leftChar - 'a']--;
                 left++;
             }
+
             right++;
         }
-        return res;
+
+        return total;
+    }
+
+private:
+    bool hasAllChars(vector<int>& freq) {
+        // Check if we have at least one of each character
+        return freq[0] > 0 && freq[1] > 0 && freq[2] > 0;
     }
 };
