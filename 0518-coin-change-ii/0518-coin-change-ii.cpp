eff5a6ee092d0341e891e0efd7cb6175ca258008
Skull-crusher44 <135740779+Skull-crusher44@users.noreply.github.com>
2025-04-16 19:28:50 +0530

    Time: 15 ms (75.42%), Space: 28.8 MB (29.9%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
-    int count(int amount, int n, vector<int>&coins ){
+    int count(int amount, int n, vector<int>&coins, vector<vector<int>>&memo  ){
         if(amount==0) // when amount =0 then there will one empty subset whose sum will be zero 
             return 1;
         if(n==0) // here no element is there => there is no way to get a subset whose sum is non zero 
             return 0;
-        int notTaken = count(amount,n-1,coins);
+
+        //check in memo
+        if(memo[n][amount]!=-1)
+            return memo[n][amount];
+        int notTaken = count(amount,n-1,coins,memo);
         
         int taken = 0;
         if(coins[n-1]<=amount)
         {
-            taken = count(amount-coins[n-1], n, coins);
+            taken = count(amount-coins[n-1], n, coins,memo);
         }
 
-        return taken + notTaken;
+        return memo[n][amount] = taken + notTaken;
         
     }
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
-        return count(amount,n,coins);      
+        vector<vector<int>>memo(n+1,vector<int>(amount+1,-1));
+        return count(amount,n,coins,memo);      
     }
 };
