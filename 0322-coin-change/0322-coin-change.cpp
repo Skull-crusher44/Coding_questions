795e9df5f8e517a26693a8d984f37c8bba98e99b
Skull-crusher44 <135740779+Skull-crusher44@users.noreply.github.com>
2025-04-16 19:10:59 +0530

    Time: 55 ms (19.83%), Space: 53.1 MB (12.1%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -20,29 +20,30 @@ public:
     
     int coinChange(vector<int>& coins, int amount) {
         int n= coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,INT_MAX));
-
+        vector<vector<int>>dp(n+1,vector<int>(amount+1,INT_MAX));
+        dp[0][0]=0;
         //intialize first row considering first element when i=0 (coins[0]) with 0<=j<=amount
         // 0 1 2 3 4 5 6 7 8 9 10 11
         // 0 1 2 3 4 5 6 7 8 9 10 11
-        for(int i=0;i<=amount;i++){
-            if(i%coins[0]==0){
-                dp[0][i]=i/coins[0];
-            }
-        }
-        for(int i =1 ; i<n ; i++)
+        // for(int i=0;i<=amount;i++){
+        //     if(i%coins[0]==0){
+        //         dp[0][i]=i/coins[0];
+        //     }
+        // }
+
+        for(int i =1 ; i<=n ; i++)
         {
             for(int j=0;j<=amount;j++){
                 int notTaken = dp[i-1][j];
                 int taken=INT_MAX;
-                if(coins[i]<=j){
-                    taken = dp[i][j-coins[i]];
+                if(coins[i-1]<=j){
+                    taken = dp[i][j-coins[i-1]];
                     if(taken!=INT_MAX)taken+=1;
                 }
                 dp[i][j] = min(taken,notTaken);
             }
         } 
         
-        return dp[n-1][amount]==INT_MAX?-1:dp[n-1][amount];    
+        return dp[n][amount]==INT_MAX?-1:dp[n][amount];    
     }
 };
