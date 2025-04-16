6a023860e831e6ba6cf4c64afe674ce971362f4a
Skull-crusher44 <135740779+Skull-crusher44@users.noreply.github.com>
2025-04-16 19:59:25 +0530

    Time: 3 ms (95.2%), Space: 10.2 MB (83.45%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -24,19 +24,17 @@ public:
         
     }
     int change(int amount, vector<int>& coins) {
-        int n=coins.size();
-        // vector<vector<int>>memo(n,vector<int>(amount+1,-1));
-        vector<vector<int>>dp(n+1,vector<int>(amount+1,0));
-        dp[0][0]=1;
-        for(int i=1;i<=n;i++){
-            for(int j=0;j<=amount;j++){
-                int notTaken = dp[i-1][j];
-                int taken=0;
-                if(coins[i-1]<=j)
-                    taken = dp[i][j-coins[i-1]];
-                dp[i][j] = taken+notTaken;
+        int n = coins.size();
+        vector<unsigned long long >dp(amount+1,0);
+        dp[0] = 1;
+        
+        for(int coin: coins){
+            for(int j = coin ;j<=amount;j++){
+                if(coin<=j)
+                    dp[j] = dp[j] + dp[j - coin]; //notTaken + taken
             }
         }
-        return dp[n][amount];      
+
+        return (int)dp[amount]; 
     }
 };
