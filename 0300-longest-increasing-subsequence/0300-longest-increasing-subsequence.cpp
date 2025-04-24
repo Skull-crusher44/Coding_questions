4e9e27020bfe39864f8bde31ef16c397c93a0100
Skull-crusher44 <135740779+Skull-crusher44@users.noreply.github.com>
2025-04-24 17:31:22 +0530

    Time: 472 ms (22.58%), Space: 296.6 MB (5.7%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    int lis(vector<int>& nums, int curr, int prev, vector<vector<int>>& dp) {
+    int lis(vector<int>& nums, int curr, int prev_index, vector<vector<int>>& dp) {
         if (curr == 0) return 0;
 
-        if (dp[curr][prev + 1] != -1)
-            return dp[curr][prev + 1];
+        if (dp[curr][prev_index] != -1)
+            return dp[curr][prev_index];
 
-        int notTaken = lis(nums, curr - 1, prev, dp);
+        int notTaken = lis(nums, curr - 1, prev_index, dp);
         int taken = 0;
-        if (prev == -1 || nums[curr - 1] < nums[prev]) {
+        if (nums[curr - 1] < nums[prev_index]) {
             taken = 1 + lis(nums, curr - 1, curr - 1, dp);
         }
 
-        return dp[curr][prev + 1] = max(taken, notTaken);
+        return dp[curr][prev_index] = max(taken, notTaken);
     }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
+        nums.push_back(INT_MAX);
 
         vector<vector<int>>dp(n+1,vector<int>(n+2,-1));
-        return lis(nums, n, -1,dp);
+        return lis(nums, n, n,dp);
     }
 };
