03bbb95de2e616454be623ad83be7ccd3aa4feb5
Skull-crusher44 <135740779+Skull-crusher44@users.noreply.github.com>
2025-06-01 18:22:57 +0530

    Time: 0 ms (100%), Space: 9.7 MB (10.99%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
-    vector<string> pad{"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-    void  solve(string digits,vector<string>&ans,string output,int index=0){
-        if(index>=digits.size())
-        {
-          ans.push_back(output);
-          return;  
-        }
-        int num= (digits[index]-'0');
-        string code=pad[num];
-        for(int i=0;i<code.length();i++)
-        {
-            output.push_back(code[i]);
-            solve(digits,ans,output,index+1);
-            output.pop_back();
+    vector<string>ans;
+    void backtrack(string digits, string temp, int index,vector<vector<char>>&map){
+        if(index>=digits.size()){
+            ans.push_back(temp);
+            return;
+        } 
 
+        //code
+        int ele = digits[index]-'0';
+        for(int i =0 ; i < map[ele].size() ; i++)
+        {
+            backtrack(digits,temp+map[ele][i],index+1,map);
         }
     }
     vector<string> letterCombinations(string digits) {
-        vector<string> ans;
-        string output;
-        if(digits.length()==0)
+        //store the mapping of digits to character
+        if(digits.empty())return ans;
+        vector<vector<char>>mapping = {
+        {'*'},
+        {'*'},
+        {'a','b','c'},
+        {'d','e','f'},
+        {'g','h','i'},
+        {'j','k','l'},
+        {'m','n','o'},
+        {'p','q','r','s'},
+        {'t','u','v'},
+        {'w','x','y','z'}
+        };
+        backtrack(digits,"",0,mapping);
         return ans;
-        solve(digits,ans,output);
-        return ans;
-        
     }
 };
